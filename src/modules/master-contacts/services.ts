509f01f71a210c80f6321e001ae36fce0e732d5e
Sameer <sbagwan@texto.in>
2026-06-15 14:02:16 +0530

    feat: add API service modules for account groups and master contacts

--- src/modules/master-contacts/services.ts
+++ src/modules/master-contacts/services.ts
@@ -20,11 +20,12 @@ const withUserId = <T extends Record<string, unknown>>(data: T) => ({ ...data, f
 export const masterContactsApi = {
   categories: {
     list: async () =>
-      (await axiosClient.get<{ data: CategoryDto[] }>('/master-contacts/categories')).data.data,
+      (await axiosClient.get<{ data: CategoryDto[] }>('/master/master-contacts/categories')).data
+        .data,
     create: async (data: CategoryDto) =>
       (
         await axiosClient.post<{ data: CategoryDto }>(
-          '/master-contacts/categories',
+          '/master/master-contacts/categories',
           withUserId(data),
         )
       ).data.data,
@@ -39,11 +40,12 @@ export const masterContactsApi = {
   },
   departments: {
     list: async () =>
-      (await axiosClient.get<{ data: DepartmentDto[] }>('/master-contacts/departments')).data.data,
+      (await axiosClient.get<{ data: DepartmentDto[] }>('/master/master-contacts/departments')).data
+        .data,
     create: async (data: DepartmentDto) =>
       (
         await axiosClient.post<{ data: DepartmentDto }>(
-          '/master-contacts/departments',
+          '/master/master-contacts/departments',
           withUserId(data),
         )
       ).data.data,
@@ -58,12 +60,12 @@ export const masterContactsApi = {
   },
   designations: {
     list: async () =>
-      (await axiosClient.get<{ data: DesignationDto[] }>('/master-contacts/designations')).data
-        .data,
+      (await axiosClient.get<{ data: DesignationDto[] }>('/master/master-contacts/designations'))
+        .data.data,
     create: async (data: DesignationDto) =>
       (
         await axiosClient.post<{ data: DesignationDto }>(
-          '/master-contacts/designations',
+          '/master/master-contacts/designations',
           withUserId(data),
         )
       ).data.data,
@@ -78,12 +80,15 @@ export const masterContactsApi = {
   },
   qualifications: {
     list: async () =>
-      (await axiosClient.get<{ data: QualificationDto[] }>('/master-contacts/qualifications')).data
-        .data,
+      (
+        await axiosClient.get<{ data: QualificationDto[] }>(
+          '/master/master-contacts/qualifications',
+        )
+      ).data.data,
     create: async (data: QualificationDto) =>
       (
         await axiosClient.post<{ data: QualificationDto }>(
-          '/master-contacts/qualifications',
+          '/master/master-contacts/qualifications',
           withUserId(data),
         )
       ).data.data,
@@ -98,12 +103,12 @@ export const masterContactsApi = {
   },
   relationships: {
     list: async () =>
-      (await axiosClient.get<{ data: RelationshipDto[] }>('/master-contacts/relationships')).data
-        .data,
+      (await axiosClient.get<{ data: RelationshipDto[] }>('/master/master-contacts/relationships'))
+        .data.data,
     create: async (data: RelationshipDto) =>
       (
         await axiosClient.post<{ data: RelationshipDto }>(
-          '/master-contacts/relationships',
+          '/master/master-contacts/relationships',
           withUserId(data),
         )
       ).data.data,
@@ -118,10 +123,14 @@ export const masterContactsApi = {
   },
   titles: {
     list: async () =>
-      (await axiosClient.get<{ data: TitleDto[] }>('/master-contacts/title')).data.data,
+      (await axiosClient.get<{ data: TitleDto[] }>('/master/master-contacts/title')).data.data,
     create: async (data: TitleDto) =>
-      (await axiosClient.post<{ data: TitleDto }>('/master-contacts/title', withUserId(data))).data
-        .data,
+      (
+        await axiosClient.post<{ data: TitleDto }>(
+          '/master/master-contacts/title',
+          withUserId(data),
+        )
+      ).data.data,
     update: async (id: number, data: Partial<TitleDto>) =>
       (await axiosClient.put<{ data: TitleDto }>(`/master-contacts/title/${id}`, withUserId(data)))
         .data.data,
@@ -129,11 +138,11 @@ export const masterContactsApi = {
   },
   city: {
     list: async () =>
-      (await axiosClient.get<{ data: { data: CityDto[] } }>('/master-contacts/city')).data.data
-        .data,
+      (await axiosClient.get<{ data: { data: CityDto[] } }>('/master/master-contacts/city')).data
+        .data.data,
     create: async (data: CityDto) =>
-      (await axiosClient.post<{ data: CityDto }>('/master-contacts/city', withUserId(data))).data
-        .data,
+      (await axiosClient.post<{ data: CityDto }>('/master/master-contacts/city', withUserId(data)))
+        .data.data,
     update: async (id: string | number, data: Partial<CityDto>) =>
       (await axiosClient.put<{ data: CityDto }>(`/master-contacts/city/${id}`, withUserId(data)))
         .data.data,
@@ -141,11 +150,15 @@ export const masterContactsApi = {
   },
   address: {
     list: async () =>
-      (await axiosClient.get<{ data: { data: AddressDto[] } }>('/master-contacts/address')).data
-        .data.data,
+      (await axiosClient.get<{ data: { data: AddressDto[] } }>('/master/master-contacts/address'))
+        .data.data.data,
     create: async (data: AddressDto) =>
-      (await axiosClient.post<{ data: AddressDto }>('/master-contacts/address', withUserId(data)))
-        .data.data,
+      (
+        await axiosClient.post<{ data: AddressDto }>(
+          '/master/master-contacts/address',
+          withUserId(data),
+        )
+      ).data.data,
     update: async (id: number, data: Partial<AddressDto>) =>
       (
         await axiosClient.put<{ data: AddressDto }>(
@@ -157,22 +170,26 @@ export const masterContactsApi = {
   },
   organizationsDropdown: {
     list: async () =>
-      (await axiosClient.get<{ data: any[] }>('/master-contacts/address/organizations/dropdown'))
-        .data.data,
+      (
+        await axiosClient.get<{ data: any[] }>(
+          '/master/master-contacts/address/organizations/dropdown',
+        )
+      ).data.data,
     create: async () => ({}) as any,
     update: async () => ({}) as any,
     remove: async () => {},
   },
   countryDropdown: {
     list: async () =>
-      (await axiosClient.get<{ data: any[] }>('/master-contacts/country/dropdown')).data.data,
+      (await axiosClient.get<{ data: any[] }>('/master/master-contacts/country/dropdown')).data
+        .data,
     create: async () => ({}) as any,
     update: async () => ({}) as any,
     remove: async () => {},
   },
   stateDropdown: {
     list: async () =>
-      (await axiosClient.get<{ data: any[] }>('/master-contacts/state/dropdown')).data.data,
+      (await axiosClient.get<{ data: any[] }>('/master/master-contacts/state/dropdown')).data.data,
     create: async () => ({}) as any,
     update: async () => ({}) as any,
     remove: async () => {},
@@ -181,58 +198,71 @@ export const masterContactsApi = {
     list: async () =>
       (
         await axiosClient.get<{ data: { rows: ModeOfContactDto[] } }>(
-          '/master-contacts/mode-of-contact',
+          '/master/master-contacts/mode-of-contact',
         )
       ).data.data.rows,
     create: async (data: ModeOfContactDto) =>
       (
         await axiosClient.post<{ data: ModeOfContactDto }>(
-          '/master-contacts/mode-of-contact',
+          '/master/master-contacts/mode-of-contact',
           withUserId(data),
         )
       ).data.data,
     update: async (id: number, data: Partial<ModeOfContactDto>) =>
       (
         await axiosClient.put<{ data: ModeOfContactDto }>(
-          `/master-contacts/mode-of-contact/${id}`,
+          `/master/master-contacts/mode-of-contact/${id}`,
           withUserId(data),
         )
       ).data.data,
-    remove: async (id: number) => axiosClient.delete(`/master-contacts/mode-of-contact/${id}`),
+    remove: async (id: number) =>
+      axiosClient.delete(`/master/master-contacts/mode-of-contact/${id}`),
   },
   state: {
     list: async () =>
-      (await axiosClient.get<{ data: { rows: StateDto[] } }>('/master-contacts/state')).data.data
-        .rows,
+      (await axiosClient.get<{ data: { rows: StateDto[] } }>('/master/master-contacts/state')).data
+        .data.rows,
     create: async (data: StateDto) =>
-      (await axiosClient.post<{ data: StateDto }>('/master-contacts/state', withUserId(data))).data
-        .data,
-    update: async (id: number, data: Partial<StateDto>) =>
-      (await axiosClient.put<{ data: StateDto }>(`/master-contacts/state/${id}`, withUserId(data)))
-        .data.data,
-    remove: async (id: number) => axiosClient.delete(`/master-contacts/state/${id}`),
-  },
-  region: {
-    list: async () =>
-      (await axiosClient.get<{ data: { rows: RegionDto[] } }>('/master-contacts/region')).data.data
-        .rows,
-    create: async (data: RegionDto) =>
-      (await axiosClient.post<{ data: RegionDto }>('/master-contacts/region', withUserId(data)))
-        .data.data,
-    update: async (id: number, data: Partial<RegionDto>) =>
       (
-        await axiosClient.put<{ data: RegionDto }>(
-          `/master-contacts/region/${id}`,
+        await axiosClient.post<{ data: StateDto }>(
+          '/master/master-contacts/state',
           withUserId(data),
         )
       ).data.data,
-    remove: async (id: number) => axiosClient.delete(`/master-contacts/region/${id}`),
+    update: async (id: number, data: Partial<StateDto>) =>
+      (
+        await axiosClient.put<{ data: StateDto }>(
+          `/master/master-contacts/state/${id}`,
+          withUserId(data),
+        )
+      ).data.data,
+    remove: async (id: number) => axiosClient.delete(`/master/master-contacts/state/${id}`),
+  },
+  region: {
+    list: async () =>
+      (await axiosClient.get<{ data: { rows: RegionDto[] } }>('/master/master-contacts/region'))
+        .data.data.rows,
+    create: async (data: RegionDto) =>
+      (
+        await axiosClient.post<{ data: RegionDto }>(
+          '/master/master-contacts/region',
+          withUserId(data),
+        )
+      ).data.data,
+    update: async (id: number, data: Partial<RegionDto>) =>
+      (
+        await axiosClient.put<{ data: RegionDto }>(
+          `/master/master-contacts/region/${id}`,
+          withUserId(data),
+        )
+      ).data.data,
+    remove: async (id: number) => axiosClient.delete(`/master/master-contacts/region/${id}`),
   },
   mocTypesDropdown: {
     list: async () =>
       (
         await axiosClient.get<{ data: ModeOfContactTypeDto[] }>(
-          '/master-contacts/mode-of-contact/types',
+          '/master/master-contacts/mode-of-contact/types',
         )
       ).data.data,
     create: async () => ({}) as any,
